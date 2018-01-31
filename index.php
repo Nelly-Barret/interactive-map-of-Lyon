570ac2814c6e6d547f9fe122f7e10ca1282bf986
Louis <louis.lebrun75@gmail.com>
2018-01-31 14:49:38 +0100

    As is

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <title></title>
+
+    <!-- Mapbox -->
+
+    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.0/mapbox-gl.js'></script>
+
+    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.0/mapbox-gl.css' rel='stylesheet' />
+
+
+    <!-- Google Places -->
+
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBPv7VFQeI_dNWfE-Y8IXIywMzEzSkNd28&libraries=places"></script>
+
+</head>
+
+<body>
+
+<div id='map' style='width: 100%; height: 100%;'></div>
+
+<script src="js/map.js"></script>
+
+
+</body>
+</html>
