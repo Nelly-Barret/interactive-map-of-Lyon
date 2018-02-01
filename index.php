f7ff3803172e811ffba2d34ea00e756628643362
nellyb <nbarret69@gmail.com>
2018-02-01 23:00:43 +0100

    Navbar style
    - Added transparence to navbar
    - Repositionning the navbar

--- index.php
+++ index.php
@@ -4,39 +4,56 @@
 		<meta charset="UTF-8">
 		
 		<title>Navbar</title>
+
+		<!-- Don't change the order else it won't work -->
 		
+		<!-- ?? -->
 		<link href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" rel="stylesheet">
 
+		<!-- Bootstrap framework (CSS) -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+		<!-- our CSS -->
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
 
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<!-- font-awsome for icons (search...) -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+		<!-- ?? -->
+		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+		
+		<!-- Bootstrap framework (JQuery) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+		<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+		
+		<!-- Bootstrap framework (JS) -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-	    <!-- Mapbox -->
+		<!-- ?? -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
-	    <script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.0/mapbox-gl.js'></script>
+		<!-- ?? -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		
 
-	    <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.0/mapbox-gl.css' rel='stylesheet' />
 
+		<!-- Mapbox -->
+		<script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.0/mapbox-gl.js'></script>
 
-	    <!-- Google Places -->
+		<link href='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.0/mapbox-gl.css' rel='stylesheet' />
+		
 
-	    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBPv7VFQeI_dNWfE-Y8IXIywMzEzSkNd28&libraries=places"></script>
-
+		<!-- Google Places -->
+		<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBPv7VFQeI_dNWfE-Y8IXIywMzEzSkNd28&libraries=places"></script>
 	</head>
 
 	<body class="container-fluid">
-			
-		<!-- static/header.php -->
 		<?php include("static/header.php"); ?>
-			
-	<div id='map' style='width: 100%; height: 100%;'></div>
+		
+		<div id='map' style='width: 100%; height: 100%;'>
+		</div>
 
-	<script src="js/map.js"></script>
-
-
+		<script src="js/map.js"></script>
 	</body>
 </html>
--- static/header.php
+++ static/header.php
@@ -1,14 +1,16 @@
 <!-- navbar-expand-md defined the limit between the expandable navbar and the mobile version -->
+
 <nav class="navbar navbar-expand-md navbar-light bg-light" id="navMenu" role="navigation">
   <!-- the button is available if it's a mobile size -->
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse" id="navbarContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item">
-        <a class="nav-link" href="#">Home</a>
+<!-- reseting the settings -->
+        <button class="btn btn-secondary" href="">Home</button>
       </li>
       <li>
         <div class="dropdown col-lg-5">
@@ -16,6 +18,7 @@
             Restaurant
           </button>
           <div class="dropdown-menu" aria-labelledby="dropdownRestaurant">
+            <!-- filters for restaurants -->
             <a class="dropdown-item" href="#">Around me</a>
             <a class="dropdown-item" href="#">Rating</a>
             <a class="dropdown-item" href="#">Price</a>
@@ -30,6 +33,7 @@
             Bar
           </button>
           <div class="dropdown-menu" aria-labelledby="dropdownBar">
+            <!-- filters for bars-->
             <a class="dropdown-item" href="#">Around me</a>
             <a class="dropdown-item" href="#">Rating</a>
             <a class="dropdown-item" href="#">Price</a>
@@ -38,9 +42,11 @@
         </div>
       </li>
     </ul>
+
+    <!-- search box -->
     <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      <button class="btn btn-success my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
     </form>
   </div>
 </nav>
