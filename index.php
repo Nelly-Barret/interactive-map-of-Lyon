70eb7f28a0a6e79355e00bc788dbede88d061af2
nellyb <nbarret69@gmail.com>
2018-02-02 21:26:47 +0100

    Responsive navbar
    - changed navbar to be responsive
    - changed search design

--- index.php
+++ index.php
@@ -7,36 +7,33 @@
 
 		<!-- Don't change the order else it won't work -->
 		
-		<!-- ?? -->
-		<link href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" rel="stylesheet">
-
-		<!-- Bootstrap framework (CSS) -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-		<!-- our CSS -->
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<!-- Latest compiled and minified CSS (Bootstrap) -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 
 		<!-- font-awsome for icons (search...) -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+		<!-- our CSS -->
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+		<!-- ?? -->
+		<link href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" rel="stylesheet">
+
 		<!-- ?? -->
 		<link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
-		
-		<!-- Bootstrap framework (JQuery) -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-		<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-		<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-		
-		<!-- Bootstrap framework (JS) -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 		<!-- ?? -->
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		
 		<!-- ?? -->
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		
-
 
 		<!-- Mapbox -->
 		<script src='https://api.tiles.mapbox.com/mapbox-gl-js/v0.44.0/mapbox-gl.js'></script>
@@ -51,7 +48,7 @@
 	<body class="container-fluid">
 		<?php include("static/header.php"); ?>
 		
-		<div id='map' style='width: 100%; height: 100%;'>
+		<div id="map" style="width: 100%; height: 100%;">
 		</div>
 
 		<script src="js/map.js"></script>
--- static/header.php
+++ static/header.php
@@ -1,52 +1,78 @@
-<!-- navbar-expand-md defined the limit between the expandable navbar and the mobile version -->
+<!-- navbar-expand-sm defines the limit between the expandable navbar and the collapse version -->
 
-<nav class="navbar navbar-expand-md navbar-light bg-light" id="navMenu" role="navigation">
-  <!-- the button is available if it's a mobile size -->
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<nav class="navbar navbar-expand-sm navbar-light bg-light" id="navMenu" role="navigation">
+	<!-- the button is available if it's a mobile size -->
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
 
-  <div class="collapse navbar-collapse" id="navbarContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
+	<div class="collapse navbar-collapse" id="navbarContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item">
 <!-- reseting the settings -->
-        <button class="btn btn-secondary" href="">Home</button>
-      </li>
-      <li>
-        <div class="dropdown col-lg-5">
-          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownRestaurant" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Restaurant
-          </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownRestaurant">
-            <!-- filters for restaurants -->
-            <a class="dropdown-item" href="#">Around me</a>
-            <a class="dropdown-item" href="#">Rating</a>
-            <a class="dropdown-item" href="#">Price</a>
-            <a class="dropdown-item" href="#">Type</a>
-          </div>
-        </div>
-      </li>
+				<button class="btn btn-secondary" href="">Home</button>
+			</li>
 
-      <li>
-        <div class="dropdown col-lg-5">
-          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownBar" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Bar
-          </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownBar">
-            <!-- filters for bars-->
-            <a class="dropdown-item" href="#">Around me</a>
-            <a class="dropdown-item" href="#">Rating</a>
-            <a class="dropdown-item" href="#">Price</a>
-            <a class="dropdown-item" href="#">Type</a>
-          </div>
-        </div>
-      </li>
-    </ul>
+			<li class="nav-item">
+				<div class="dropdown">
+					<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownBar" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Bar
+					</button>
+					<div class="dropdown-menu" aria-labelledby="dropdownBar">
+						<!-- filters for bars -->
+						<a class="dropdown-item" href="#">Around me</a>
+						<a class="dropdown-item" href="#">Rating</a>
+						<a class="dropdown-item" href="#">Price</a>
+						<a class="dropdown-item" href="#">Type</a>
+					</div>
+				</div>
+			</li>
 
-    <!-- search box -->
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-success my-2 my-sm-0" type="submit"><i class="fa fa-search"></i></button>
-    </form>
-  </div>
+			<li class="nav-item">
+				<div class="dropdown">
+					<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownRestaurant" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						Restaurant
+					</button>
+					<div class="dropdown-menu" aria-labelledby="dropdownRestaurant">
+						<!-- filters for restaurants -->
+						<a class="dropdown-item" href="#">Around me</a>
+						<a class="dropdown-item" href="#">Rating</a>
+						<a class="dropdown-item" href="#">Price</a>
+						<a class="dropdown-item" href="#">Type</a>
+					</div>
+				</div>
+			</li>
+
+			<!-- search box -->
+			<li class="nav-item">
+<!-- my-2 my-lg-0 -->
+				<form class="form-inline"> 
+					<!-- data-target argument call the part of the code for displaying the modal -->
+					<button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">
+						Search <i class="fa fa-search"></i>
+					</button>
+				</form>
+			</li>
+		</ul>
+	</div>
 </nav>
+
+<!-- modal for search -->
+<div class="modal fade" id="myModal" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title">Search</h4>
+			</div>
+
+			<div class="modal-body">
+				<input class="form-control mr-sm-2" type="search" placeholder="Type here your search..." aria-label="Search">
+			</div>
+
+			<div class="modal-footer">
+				<button type="button" class="btn btn-success">Search</button>
+				<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
