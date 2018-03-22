bdd587b732f1a68da2ccc8224c3d41aef18a16c7
nellyb <nbarret69@gmail.com>
2018-03-22 22:39:13 +0100

    Some design
    - Change button search
    - Change a little CSS

--- static/navbar.php
+++ static/navbar.php
@@ -10,15 +10,33 @@
             <div class="dropdown-menu" id="dropdownFilters">
                 <div class="dropdown-item">
                     <label for="typeContainer">Type</label>
-                    <div class="containerButtons" id="typeContainer"><button id="restaurantButton" type="button" class="btnType btn">Restaurant</button><button id="barButton" type="button" class="btnType btn">Bar</button><button id="barRestaurantButton" type="button" class="btnType btn">Bar-Restaurant</button><button id="restaurantButton" type="button" class="btnRemove btn"><i class="fa fa-remove"></i></button></div>
+                    <div class="containerButtons" id="typeContainer">
+                        <button id="restaurantButton" type="button" class="btnType btn">Restaurant</button>
+                        <button id="barButton" type="button" class="btnType btn">Bar</button>
+                        <button id="barRestaurantButton" type="button" class="btnType btn">Bar-Restaurant</button>
+                        <button id="restaurantButton" type="button" class="btnRemove btn"><i class="fa fa-remove"></i></button>
+                    </div>
                 </div>
                 <div class="dropdown-item">
                     <label for="currencyContainer">Price</label>
-                    <div class="containerButtons" id="currencyContainer"><button id="priceButton1" type="button" class="btnPrice btn"><i class="fa fa-euro"></i></button><button id="priceButton2" type="button" class="btnPrice btn"><i class="fa fa-euro"></i><i class="fa fa-euro"></i></button><button id="priceButton3" type="button" class="btnPrice btn"><i class="fa fa-euro"></i><i class="fa fa-euro"></i><i class="fa fa-euro"></i></button><button id="priceButton4" type="button" class="btnPrice btn"><i class="fa fa-euro"></i><i class="fa fa-euro"></i><i class="fa fa-euro"></i><i class="fa fa-euro"></i></button><button id="restaurantButton" type="button" class="btnRemove btn"><i class="fa fa-remove"></i></button></div>
+                    <div class="containerButtons" id="currencyContainer">
+                        <button id="priceButton1" type="button" class="btnPrice btn"><i class="fa fa-euro"></i></button>
+                        <button id="priceButton2" type="button" class="btnPrice btn"><i class="fa fa-euro"></i><i class="fa fa-euro"></i></button>
+                        <button id="priceButton3" type="button" class="btnPrice btn"><i class="fa fa-euro"></i><i class="fa fa-euro"></i><i class="fa fa-euro"></i></button>
+                        <button id="priceButton4" type="button" class="btnPrice btn"><i class="fa fa-euro"></i><i class="fa fa-euro"></i><i class="fa fa-euro"></i><i class="fa fa-euro"></i></button>
+                        <button id="restaurantButton" type="button" class="btnRemove btn"><i class="fa fa-remove"></i></button>
+                    </div>
                 </div>
                 <div class="dropdown-item">
                     <label for="starContainer">Rating</label>
-                    <div class="containerButtons" id="starContainer"><button type="button" id="starButton1" class="btnStar btn"><span class="fa fa-star"></span></button><button type="button" id="starButton2" class="btnStar btn"><span class="fa fa-star"></span></button><button type="button" id="starButton3" class="btnStar btn"><span class="fa fa-star"></span></button><button type="button" id="starButton4" class="btnStar btn"><span class="fa fa-star"></span></button><button type="button" id="starButton5" class="btnStar btn"><span class="fa fa-star"></span></button><button id="restaurantButton" type="button" class="btnRemove btn"><i class="fa fa-remove"></i></button></div>
+                    <div class="containerButtons" id="starContainer">
+                        <button type="button" id="starButton1" class="btnStar btn"><span class="fa fa-star"></span></button>
+                        <button type="button" id="starButton2" class="btnStar btn"><span class="fa fa-star"></span></button>
+                        <button type="button" id="starButton3" class="btnStar btn"><span class="fa fa-star"></span></button>
+                        <button type="button" id="starButton4" class="btnStar btn"><span class="fa fa-star"></span></button>
+                        <button type="button" id="starButton5" class="btnStar btn"><span class="fa fa-star"></span></button>
+                        <button id="restaurantButton" type="button" class="btnRemove btn"><i class="fa fa-remove"></i></button>
+                    </div>
                 </div>
                 <div class="dropdown-item">
                     <div class="form-check checkbox-slider--b">
@@ -66,10 +84,15 @@
             </div>
             <div class="modal-body">
                 <label id="mapStyleButton" for="containerViews">Map style</label>
-                <div id="containerViews" class="containerStyles"><button type="button" class="btn">Streets</button><button type="button" class="btn">Light</button><button type="button" class="btn">Dark</button><button type="button" class="btn">Satellite</button></div>
+                <div id="containerViews" class="containerStyles">
+                    <button type="button" class="btn">Streets</button><button type="button" class="btn">Light</button><button type="button" class="btn">Dark</button><button type="button" class="btn">Satellite</button>
+                </div>
 
-                <label id="clusterButton" for="containerViews">Map style</label>
-                <div id="containerClusters"><input type="radio" id="viewClusters"><label id="clusterButton" for="viewClusters">Clusters</label><input type="radio" id="viewPoints"><label id="clusterButton" for="viewPoints">Points</label></div>
+                <label id="clusterButton" for="containerViews">Display</label>
+                <div id="containerClusters">
+                    <input type="radio" name="viewClust" id="viewPoints" checked="checked"><label id="clusterButton" for="viewPoints"> Points</label>
+                    <input type="radio" name="viewClust" id="viewClusters"><label id="clusterButton" for="viewClusters"> Clusters</label>
+                </div>
 
             </div>
             <div class="modal-footer">
