6990fb96967c978526061ac165222c34f1f88fc6
Louis <louis.lebrun75@gmail.com>
2018-04-03 18:18:01 +0200

    Added crash correction on popup

--- js/map.js
+++ js/map.js
@@ -817,15 +817,19 @@ function createMarkerPopupHTML(place) {
 
             console.log(str);
 
-            if( forcedDate == null ) {
+            if (str != null && str.length != 0){
 
-                //str.indexOf( ': ' )+2 => starts after ': '
-                html += "<p class='day card-body'><i class='fa fa-clock-o'></i>Today: " + str.substring(str.indexOf(': ') + 2, str.length) + "</p>";
+                if( forcedDate == null ) {
 
-            } else {
+                    //str.indexOf( ': ' )+2 => starts after ': '
+                    html += "<p class='day card-body'><i class='fa fa-clock-o'></i>Today: " + str.substring(str.indexOf(': ') + 2, str.length) + "</p>";
 
-                //str.indexOf( ': ' )+2 => starts after ': '
-                html += "<p class='day card-body'><i class='fa fa-clock-o'></i>" + day_fold(str.substring(0, str.indexOf(':'))) + str.substring(str.indexOf(':'), str.length) + "</p>\n";
+                } else {
+
+                    //str.indexOf( ': ' )+2 => starts after ': '
+                    html += "<p class='day card-body'><i class='fa fa-clock-o'></i>" + day_fold(str.substring(0, str.indexOf(':'))) + str.substring(str.indexOf(':'), str.length) + "</p>\n";
+
+                }
 
             }
 
